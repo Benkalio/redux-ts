4faefdf26a7b2ffa7a25f385efa7c8fa01a16bb0
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2022-05-02 21:21:13 +0100

    applied typings to dispatch

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,31 +1,35 @@
 import axios from "axios";
+import { Dispatch } from "redux";
 import { ActionType } from "../action-types";
 import { Action } from "../actions";
 
 export const searchRepositories = (term: string) => {
-  return async (dispatch: any) => {
+  return async (dispatch: Dispatch<Action>) => {
     dispatch({
       type: ActionType.SEARCH_REPOSITORIES
     });
     try {
-      const { data } = await axios.get('https://registry.npmjs.org/-/v1/search', {
-        params: {
-          text: term
+      const { data } = await axios.get(
+        'https://registry.npmjs.org/-/v1/search',
+        {
+          params: {
+            text: term
+          }
         }
-      });
+      );
       const names = data.objects.map((result: any) => {
         return result.package.name;
       });
       dispatch({
         type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
-        payload: names
-      })
+        payload: names,
+      });
       
-    } catch (err: any) {
+    } catch (error: any) {
       dispatch({
         type: ActionType.SEARCH_REPOSITORIES_ERROR,
-        payload: err.message
-      })
+        payload: error.message
+      });
     }
   }
 }
